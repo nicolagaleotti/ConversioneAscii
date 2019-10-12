e96a9245cf31d8eeeab307065c91bb6b621f77d6
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-12 11:32:48 +0200

    fine programma con Convert.ToInt32

--- ConversioneAscii/Program.cs
+++ ConversioneAscii/Program.cs
@@ -6,7 +6,13 @@ namespace ConversioneAscii
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserisci qualcosa: ");
+            string risposta = Console.ReadLine();
+            foreach (char c in risposta)
+            {
+                Console.WriteLine(System.Convert.ToInt32(c));
+            }
+            Console.ReadLine();
         }
     }
 }
